33e190cdb83b13b46bc4249229a4be09fa96b277
L-ui139 <2535634250@qq.com>
2026-09-11 15:42:34 +0800

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,10 @@
+python evaluation.py lasher --tracker_names mytrack --result_paths /clzs_test022/lj/long/tbsi/output/test/tracking_results/tbsi_track/vitb_256_tbsi_32x1_1e4_lasher_15ep_sot_0
+
+
+python evaluation.py rgbt234 --tracker_names mytrack --result_paths /clzs_test022/lj/long/tbsi/output1/rgbt234_test/tracking_results/tbsi_track/vitb_256_tbsi_32x1_1e4_lasher_15ep_sot_0
+
+
+python evaluation.py rgbt210 --tracker_names mytrack --result_paths /clzs_test022/lj/long/tbsi/output1/rgbt210_test/tracking_results/tbsi_track/vitb_256_tbsi_32x1_1e4_lasher_15ep_sot_0
+
+
+
